9e7f0c62e9f27e3dd6078eb8b4a53641f620567a
Jonathan <275568635@qq.com>
2025-06-09 11:42:22 +0800

    fix bug

--- src/ui/setting/propeller_conf/propeller_conf_load_limit_curve.py
+++ src/ui/setting/propeller_conf/propeller_conf_load_limit_curve.py
@@ -58,5 +58,5 @@ class PropellerConfLimitCurve(CustomCard):
         self.ps.bhp_right_of_torque_load_limit_curve = self.bhp_right_of_torque_load_limit_curve.value
         self.ps.line_color_of_torque_load_limit_curve = self.line_color_of_torque_load_limit_curve.color
 
-        gdata.speed_of_torque_load_limit = self.ps.rpm_right_of_torque_load_limit_curve
-        gdata.power_of_torque_load_limit = self.ps.bhp_right_of_torque_load_limit_curve
+        gdata.speed_of_torque_load_limit = float(self.ps.rpm_right_of_torque_load_limit_curve)
+        gdata.power_of_torque_load_limit = float(self.ps.bhp_right_of_torque_load_limit_curve)
