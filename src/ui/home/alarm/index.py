c7715db2e86836c8b3703d19a2a6a0f4f8e07fb0
孔祥明 <275568635@qq.com>
2025-06-23 20:24:47 +0800

    1

--- src/ui/home/alarm/index.py
+++ src/ui/home/alarm/index.py
@@ -101,7 +101,11 @@ class AlarmList(ft.Container):
             return
 
         for row in selected_rows:
-            AlarmLog.update(acknowledge_time=gdata.utc_date_time, is_sync = False).where(AlarmLog.id == row.cells[0].data).execute()
+            AlarmLog.update(acknowledge_time=gdata.utc_date_time, is_sync = False).where(
+                AlarmLog.id == row.cells[0].data,
+                AlarmLog.is_from_master == False,
+                AlarmLog.acknowledge_time is None
+            ).execute()
 
         self.table.search()
         Toast.show_success(self.page)
