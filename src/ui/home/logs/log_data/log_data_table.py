fc9895b1d0d2915a8b0649e4f3f8aded5765f6d1
孔祥明 <275568635@qq.com>
2025-07-21 17:36:44 +0800

    convert min unit of data to k

--- src/ui/home/logs/log_data/log_data_table.py
+++ src/ui/home/logs/log_data/log_data_table.py
@@ -1,7 +1,10 @@
+import logging
 from db.models.data_log import DataLog
 from ui.common.abstract_table import AbstractTable
 from common.global_data import gdata
 from db.models.date_time_conf import DateTimeConf
+from utils.unit_parser import UnitParser
+
 
 class LogDataTable(AbstractTable):
     def __init__(self):
@@ -10,54 +13,85 @@ class LogDataTable(AbstractTable):
         self.dtc: DateTimeConf = DateTimeConf.get()
 
     def load_total(self):
-        start_date = self.kwargs.get('start_date')
-        end_date = self.kwargs.get('end_date')
-        sql = DataLog.select()
-        if start_date and end_date:
-            sql = sql.where(DataLog.utc_date_time >= start_date, DataLog.utc_date_time <= end_date)
+        try:
+            start_date = self.kwargs.get('start_date')
+            end_date = self.kwargs.get('end_date')
+            sql = DataLog.select()
+            if start_date and end_date:
+                sql = sql.where(DataLog.utc_date_time >= start_date,
+                                DataLog.utc_date_time <= end_date)
 
-        return sql.count()
+            return sql.count()
+        except:
+            logging.exception('exception occured at LogDataTable.load_total')
+
+        return 0
 
     def load_data(self):
-        sql = DataLog.select(
-            DataLog.id,
-            DataLog.name,
-            DataLog.utc_date_time,
-            DataLog.ad_0_torque,
-            DataLog.ad_1_thrust,
-            DataLog.speed,
-            DataLog.power
-        )
-        start_date = self.kwargs.get('start_date')
-        end_date = self.kwargs.get('end_date')
-        if start_date and end_date:
-            sql = sql.where(DataLog.utc_date_time >= start_date,
-                            DataLog.utc_date_time <= end_date)
+        try:
+            sql = DataLog.select(
+                DataLog.id,
+                DataLog.name,
+                DataLog.utc_date_time,
+                DataLog.ad_0_torque,
+                DataLog.ad_1_thrust,
+                DataLog.speed,
+                DataLog.power
+            )
+            start_date = self.kwargs.get('start_date')
+            end_date = self.kwargs.get('end_date')
+            if start_date and end_date:
+                sql = sql.where(DataLog.utc_date_time >= start_date,
+                                DataLog.utc_date_time <= end_date)
 
-        data = sql.order_by(DataLog.id.desc()).paginate(
-            self.current_page, self.page_size)
+            data = sql.order_by(DataLog.id.desc()).paginate(
+                self.current_page, self.page_size)
 
-        return [[
-                item.id,
-                item.name,
-                item.utc_date_time.strftime(f'{self.dtc.date_format} %H:%M:%S'),
-                item.ad_0_torque,
-                item.ad_1_thrust,
-                item.speed,
-                item.power
-                ] for item in data]
+            data_list = []
+            for item in data:
+                torque = UnitParser.parse_torque(item.ad_0_torque, 0)
+                thrust = UnitParser.parse_thrust(item.ad_1_thrust, 0)
+                power = UnitParser.parse_power(item.power, 0)
+                speed = UnitParser.parse_speed(item.speed)
+                data_list.append([
+                    item.id,
+                    item.name,
+                    item.utc_date_time.strftime(
+                        f'{self.dtc.date_format} %H:%M:%S'),
+                    torque[0],
+                    thrust[0],
+                    speed[0],
+                    power[0]
+                ])
+
+                return data_list
+        except:
+            logging.exception('exception occured at LogDataTable.load_data')
+
+        return []
 
     def create_columns(self):
         return self.get_columns()
 
     def get_columns(self):
-        session = self.page.session
-        return [
-            session.get("lang.common.no"),
-            session.get("lang.common.propeller_name"),
-            session.get("lang.common.utc_date_time"),
-            f'{session.get("lang.common.torque")}(Nm)',
-            f'{session.get("lang.common.thrust")}(N)',
-            session.get("lang.common.speed"),
-            f'{session.get("lang.common.power")}(W)'
-        ]
+        try:    
+            if self.page is None:
+                return []
+            
+            if self.page.session is None:
+                return []
+
+            session = self.page.session
+            return [
+                session.get("lang.common.no"),
+                session.get("lang.common.propeller_name"),
+                session.get("lang.common.utc_date_time"),
+                f'{session.get("lang.common.torque")}(kNm)',
+                f'{session.get("lang.common.thrust")}(kN)',
+                f'{session.get("lang.common.speed")}(rpm)',
+                f'{session.get("lang.common.power")}(kW)'
+            ]
+        except:
+            logging.exception('exception occured at LogDataTable.get_columns')
+
+        return []
