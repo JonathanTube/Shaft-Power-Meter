cb8a4037fee1290efdca87944a50b6d4a3e71e32
Jonathan <275568635@qq.com>
2025-08-10 22:17:23 +0800

    outer_id

--- src/db/models/alarm_log.py
+++ src/db/models/alarm_log.py
@@ -15,7 +15,7 @@ class AlarmLog(BaseModel):
 
     is_from_master = BooleanField(verbose_name="是否来自于master", default=False)
 
-    out_id = IntegerField(verbose_name="外部id, 如果是slave同步的master数据, 就是master中alarmLog.id，否则相反。", null=True)
+    outer_id = IntegerField(verbose_name="外部id, 如果是slave同步的master数据, 就是master中alarmLog.id，否则相反。", null=True)
 
     class Meta:
         table_name = 'alarm_log'
