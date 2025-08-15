5791f0fa5559df45cf98efdd4a7726f812eb6336
Jonathan <275568635@qq.com>
2025-08-15 11:02:08 +0800

    123123

--- src/jm3846/JM3846_torque_rpm_util.py
+++ src/jm3846/JM3846_torque_rpm_util.py
@@ -13,8 +13,8 @@ class JM3846TorqueRpmUtil:
             # CH_SEL1\CH_SEL0 都不为4\0且SPEED_SEL=1时： ch1-rpm -ch1-rpm-;
             # CH_SEL1\CH_SEL0 都不为0\1且SPEED_SEL=1时： ch0-rpm -ch0-rpm-;
             # CH_SEL1\CH_SEL0 都不为1\1且SPEED_SEL=0时： ch0-ch1 -ch0-ch1-；
-            ch0_sum = None
-            rpm_sum = None
+            ch0_sum = 0
+            rpm_sum = 0
             ch_sel_1 = gdata.configSPS.ch_sel_1 if name == 'sps' else gdata.configSPS2.ch_sel_1
             ch_sel_0 = gdata.configSPS.ch_sel_0 if name == 'sps' else gdata.configSPS2.ch_sel_0
             speed_sel = gdata.configSPS.speed_sel if name == 'sps' else gdata.configSPS2.speed_sel
@@ -58,8 +58,8 @@ class JM3846TorqueRpmUtil:
                     if len(chunk) == channel_count:
                         ch0_sum += chunk[0]
 
-            ch0_ad = ch0_sum / part_length if ch0_sum else None
-            rpm = rpm_sum / part_length if rpm_sum else None
+            ch0_ad = ch0_sum / part_length if ch0_sum else 0
+            rpm = rpm_sum / part_length if rpm_sum else 0
 
             return (ch0_ad, rpm)
         except:
